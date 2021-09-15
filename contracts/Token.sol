610537a3d2057011e7118072fb5f6d006873c22b
Kingsley Victor <javaprodigy56@gmail.com>
2021-09-15 10:09:06 +0100

    create token contract

---
+++ contracts/Token.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
+
+contract Token is ERC20, Context {
+  constructor(
+    string memory name_,
+    string memory symbol_,
+    uint256 amount_
+  ) public ERC20(name_, symbol_) {
+    _mint(_msgSender(), amount_);
+  }
+}
