05c371b9c30fcc14224f4f1f5aa24e7e50e015c7
Kingsley Victor <javaprodigy56@gmail.com>
2021-10-19 10:02:13 +0100

    add burn and mint functions

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,13 +2,37 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/Context.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Token is Context, ERC20 {
+/// @author Kingsley Victor
+contract Token is Context, Ownable, ERC20 {
+  mapping(address => bool) _minters;
+
+  modifier onlyMinter() {
+    require(
+      _minters[_msgSender()],
+      "VeFi: Only minter is allowed to mint tokens"
+    );
+    _;
+  }
+
   constructor(
     string memory name_,
     string memory symbol_,
     uint256 amount_
-  ) public ERC20(name_, symbol_) {
+  ) Ownable() ERC20(name_, symbol_) {
     _mint(_msgSender(), amount_);
+  }
+
+  function addMinter(address _minter) external onlyOwner {
+    _minters[_minter] = true;
+  }
+
+  function mintFor(address _account, uint256 _amount) external onlyMinter {
+    _mint(_account, _amount);
+  }
+
+  function burnFor(address _account, uint256 _amount) external onlyMinter {
+    _burn(_account, _amount);
   }
 }
