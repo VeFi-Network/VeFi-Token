7ad079875ec3bd2fcd9e16a8866c5b516043ee08
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-25 00:32:07 +0100

    update contract

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -54,6 +54,25 @@ contract Token is Context, Ownable, ERC20 {
     _burn(_account, _amount);
   }
 
+  /** @dev Transfer token using selector */
+  function _safeTransfer(
+    address _token,
+    address _recipient,
+    uint256 _amount
+  ) private returns (bool) {
+    (bool success, bytes memory data) = _token.call(
+      abi.encodeWithSelector(bytes4(keccak256(bytes('transfer(address,uint256)'))), _recipient, _amount)
+    );
+    require(success && (data.length == 0 || abi.decode(data, (bool))), 'vefi: could not call via selector');
+    return true;
+  }
+
+  function _safeTransferETH(address _to, uint256 _value) private returns (bool) {
+    (bool success, ) = _to.call{value: _value}(new bytes(0));
+    require(success, 'vefi: eth transfer failed');
+    return true;
+  }
+
   /** @dev Return tokens or Ether sent to contract to specified recipient
    *  @param _token The address of the ERC20 token. If zero address, represents Ether
    *  @param _recipient The address to send asset to
@@ -70,10 +89,10 @@ contract Token is Context, Ownable, ERC20 {
       uint256 _balance = address(this).balance;
       address payable recipient_ = payable(_recipient);
       require(_balance >= _amount, 'vefi: not enough balance');
-      recipient_.transfer(_amount);
+      require(_safeTransferETH(recipient_, _amount), 'vefi: unable to transfer eth');
     } else {
       require(IERC20(_token).balanceOf(address(this)) >= _amount, 'vefi: not enough tokens');
-      require(IERC20(_token).transfer(_recipient, _amount), 'vefi: unable to transfer tokens');
+      require(_safeTransfer(_token, _recipient, _amount), 'vefi: unable to transfer tokens');
     }
   }
 
