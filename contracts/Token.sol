e7acc60efb28e77cef137bf57619380be82ce4ae
Kingsley Victor <javaprodigy56@gmail.com>
2022-04-27 10:31:34 +0100

    redeploy to brise

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -7,51 +7,12 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 
 /** @author Kingsley Victor */
 contract Token is Context, Ownable, ERC20 {
-  mapping(address => bool) _minters;
-
-  modifier onlyMinter() {
-    require(_minters[_msgSender()], 'vefi: only minter is allowed to mint or burn tokens');
-    _;
-  }
-
   constructor(
     string memory name_,
     string memory symbol_,
     uint256 amount_
   ) Ownable() ERC20(name_, symbol_) {
     _mint(_msgSender(), amount_);
-  }
-
-  /** @dev Adds a minter. Can only be called by contract owner
-   *  @param _minter The address that's added as minter
-   */
-  function addMinter(address _minter) external onlyOwner {
-    require(!_minters[_minter], 'vefi: already a minter');
-    _minters[_minter] = true;
-  }
-
-  /** @dev Removes a minter. Can only be called by contract owner
-   *  @param _minter The address that's removed as minter
-   */
-  function removeMinter(address _minter) external onlyOwner {
-    require(_minters[_minter], 'vefi: not a minter');
-    _minters[_minter] = false;
-  }
-
-  /** @dev Mints tokens for a specific address. Can only be called by a minter
-   *  @param _account The address to mint tokens for
-   *  @param _amount The amount of tokens to be minted
-   */
-  function mintFor(address _account, uint256 _amount) external onlyMinter {
-    _mint(_account, _amount);
-  }
-
-  /** @dev Burns tokens for a specific address. Can only be called by a minter
-   *  @param _account The address whose tokens should be burnt
-   *  @param _amount The amount of tokens to be burnt
-   */
-  function burnFor(address _account, uint256 _amount) external onlyMinter {
-    _burn(_account, _amount);
   }
 
   /** @dev Transfer token using selector */
